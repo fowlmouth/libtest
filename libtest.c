78c3ed24d88edcccc348ae4f9fe4585ff7657189
fowl <phowl.mouth@gmail.com>
2022-03-03 22:41:48 -0600

    first commit

---
+++ libtest.c
@@ -0,0 +1,87 @@
+#include "libtest.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+struct test_case
+{
+  const char* name;
+  libtest_testfunction fn;
+};
+
+struct test_suite
+{
+  const char* name;
+
+  int case_count, case_capacity;
+  struct test_case* cases;
+};
+
+int suite_count = 0, suite_capacity = 0;
+struct test_suite* suites = NULL;
+
+struct test_suite* get_suite(const char* name)
+{
+  for(int i = 0; i < suite_count; ++i)
+  {
+    struct test_suite* suite = suites+i;
+    if(!strcmp(name, suite->name))
+      return suite;
+  }
+  if(suite_count == suite_capacity)
+  {
+    suite_capacity = suite_capacity ? suite_capacity * 2 : 4;
+    suites = realloc(suites, sizeof(struct test_suite) * suite_capacity);
+  }
+  struct test_suite* suite = suites + suite_count++;
+  suite->name = name;
+  suite->case_count = suite->case_capacity = 0;
+  suite->cases = NULL;
+  return suite;
+}
+
+void save_case(struct test_suite* suite, const char* name, libtest_testfunction fn)
+{
+  if(suite->case_count == suite->case_capacity)
+  {
+    suite->case_capacity = suite->case_capacity ? suite->case_capacity * 2 : 4;
+    suite->cases = realloc(suite->cases, sizeof(struct test_case) * suite->case_capacity);
+  }
+  struct test_case* this_case = suite->cases + suite->case_count++;
+  this_case->name = name;
+  this_case->fn = fn;
+}
+
+int libtest_register_test(const char* testsuite, const char* testcase, libtest_testfunction fn)
+{
+  printf("registering suite='%s' case='%s'\n", testsuite, testcase);
+
+  struct test_suite* suite = get_suite(testsuite);
+  save_case(suite, testcase, fn);
+
+  return 0;
+}
+
+void libtest_run_test_suite(struct test_suite* suite);
+void libtest_run_all_suites()
+{
+  for(struct test_suite* iter = suites, *end = iter + suite_count; iter != end; ++iter)
+  {
+    libtest_run_test_suite(iter);
+  }
+}
+
+void libtest_run_test_suite(struct test_suite* suite)
+{
+  const int error_buffer_size = 1024;
+  char error_buffer[error_buffer_size];
+  for(struct test_case* iter = suite->cases, *end = iter + suite->case_count; iter != end; ++iter)
+  {
+    int failures = 0;
+    if(iter->fn(error_buffer_size, error_buffer))
+    {
+      fprintf(stderr, "%s:%s failed: %s\n", suite->name, iter->name, error_buffer);
+      ++failures;
+    }
+  }
+}
---
+++ libtest.h
@@ -0,0 +1,54 @@
+#ifndef TESTS_HEADER_INCLUDED
+#define TESTS_HEADER_INCLUDED
+
+#include <stdio.h>
+
+#define ASSERT_MSG(bool, msg) \
+  if(!(bool)) \
+    FAIL("Assertion failed: %s", (msg));
+
+#define ASSERT_OP(lhs, op, rhs) \
+  ASSERT_MSG(((lhs) op (rhs)), (#lhs #op #rhs))
+
+#define ASSERT_EQ(lhs, rhs) \
+  ASSERT_OP(lhs, ==, rhs)
+
+
+#define FAIL(msg, ...) \
+  do{ \
+    snprintf(errormsg, errormsg_size, (msg), __VA_ARGS__); \
+    return testresult_fail; \
+  }while(0)
+
+
+#define PASS() \
+  return testresult_pass;
+
+typedef enum {
+  testresult_pass, testresult_fail
+} libtest_testresult;
+
+typedef libtest_testresult (*libtest_testfunction)(int, char*);
+
+#define TEST_FUNCTION_NAME(testsuite, testcase) \
+  _test_##testsuite_##testcase
+
+#define DECL_TEST(testsuite, testcase) \
+  libtest_testresult TEST_FUNCTION_NAME(testsuite, testcase) (int errormsg_size, char* errormsg)
+
+#define TEST(testsuite, testcase) \
+  DECL_TEST(testsuite, testcase); \
+  __attribute__((constructor)) \
+  void _libtest_internal_register_##testsuite_##testcase(void) \
+  { \
+    libtest_register_test(#testsuite, #testcase, TEST_FUNCTION_NAME(testsuite, testcase)); \
+  } \
+  DECL_TEST(testsuite, testcase)
+
+#define RUN_TESTS() \
+  libtest_run_all_suites()
+
+int libtest_register_test(const char* testsuite, const char* testcase, libtest_testfunction fn);
+void libtest_run_all_suites();
+
+#endif
